17f30e2b3e49e10e434fe6b8b732db819632f8fb
agu <iba.agustin@gmail.com>
2022-07-02 16:09:38 -0300

    Header done, starting projects section

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,15 +3,29 @@ import "./Header.css";
 export default function Header() {
   return (
     <header>
-      <div className="img-container"></div>
+      <img
+        src={require("./camera.jpg")}
+        alt="woman holding camera"
+        className="img-container"
+      ></img>
       <div className="about">
-        <h3>About</h3>
+        <h3 className="about-title">About</h3>
         <p className="about-text">
-          random text about me, bla bla bla words lol
+          Thranduil conjurer cutting your kin. Feels cry Lórien prisoners
+          marched creep minds wander? Store Orcrist wanes brown purring less
+          rascals want sorceress city burns? éored woods bond birthday pale
+          evacuate Ingrates.
         </p>
+
         <ul className="about-links">
-          <li>gh</li>
-          <li>linkedin</li>
+          <li>
+            <a href="https://github.com/jinitsuga">
+              <i className="devicon-github-original colored"></i>
+            </a>
+          </li>
+          <li>
+            <i class="devicon-linkedin-plain"></i>
+          </li>
         </ul>
       </div>
     </header>
